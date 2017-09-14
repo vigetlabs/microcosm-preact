f2c8a2fe5ac2393608b468344f4e981ce51dfcf5
Nathan Hunzaker <nate.hunzaker@gmail.com>
2017-09-14 15:29:57 -0400

    Setup redirect to microcosm/packages/preact

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import ActionForm from './action-form'
-import ActionButton from './action-button'
-import Presenter from './presenter'
-import withSend from './with-send'
-
-export { ActionForm, ActionButton, Presenter, withSend }
--- test/setup.js
+++
@@ -1,5 +0,0 @@
-// Place setup behavior here
-// Force sync rendering in Preact
-import { options } from 'preact'
-
-options.debounceRendering = update => update()
